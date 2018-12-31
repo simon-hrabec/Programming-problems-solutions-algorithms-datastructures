4f71d89abc1557b2b4612e210feeb5716c9598f5
simon-hrabec <simon.hrabec@gmail.com>
2018-12-31 13:20:56 +0100

    Update 2D array template
    Add convenience methods, constructors etc.

--- ND dynamic array/array2D.cpp
+++ ND dynamic array/array2D.cpp
@@ -1,100 +1,68 @@
 #include <vector>
-#include <algorithm>
-using namespace std;
+#include <tuple>
 
 template <class T>
 class array2D{
-    vector<T> data;
-    size_t x_size, y_size;
-    
-  public:
-    array2D(size_t x, size_t y) : data(x*y), x_size(x), y_size(y) {}
-    array2D(size_t x, size_t y, T val) : data(x*y, val), x_size(x), y_size(y) {}
-    
-    T& get(size_t x, size_t y) {
-        return data[x*y_size+x]; 
-    }
-    
-    const T& get(size_t x, size_t y) const{
-        return data[x*y_size+x];
-    }
-    
-    iterator begin(){
-        return data.begin();
-    }
-    
-    iterator end(){
-        return data.end();
-    }
-    
-    const_iterator begin() const{
-        return data.begin();
-    }
-    
-    const_iterator end() const{
-        return data.end();
-    }
-}
+  std::vector<T> data;
+  size_t x_size, y_size;
+  
+public:
+  using value_type = T;
+  using iterator = typename std::vector<T>::iterator;
+  using const_iterator = typename std::vector<T>::const_iterator;
 
+  array2D(std::tuple<size_t, size_t> sizes) : array2D(std::get<0>(sizes), std::get<1>(sizes)) {}
+  array2D(std::tuple<size_t, size_t> sizes, const T &val) : array2D(std::get<0>(sizes), std::get<1>(sizes), val) {}
+  array2D(size_t x, size_t y) : data(x*y), x_size(x), y_size(y) {}
+  array2D(size_t x, size_t y, const T &val) : data(x*y, val), x_size(x), y_size(y) {}
+  array2D(size_t x, size_t y, std::vector<T> &&dataSource) : data(std::move(dataSource)), x_size(x), y_size(y) {}
+  array2D(size_t x, size_t y, std::initializer_list<T> dataSource) : data(std::move(dataSource)), x_size(x), y_size(y) {}
 
-template <class T, class DIM>
-class array2D{
-    std::array<site_t, DIM> sizes;
-    vector<T> data;
-    size_t x_size, y_size;
-    
-    //constructor
+  T& operator()(size_t x, size_t y) {
+      return get(x, y);
+  }
 
-    //get function
+  const T& operator()(size_t x, size_t y) const {
+      return get(x, y);
+  }
+  
+  T& get(size_t x, size_t y) {
+    return data[x*y_size+y]; 
+  }
+  
+  const T& get(size_t x, size_t y) const {
+    return data[x*y_size+y];
+  }
+  
+  iterator begin() {
+    return std::begin(data);
+  }
+  
+  iterator end() {
+    return std::end(data);
+  }
+  
+  const_iterator begin() const {
+    return data.begin();
+  }
+  
+  const_iterator end() const {
+    return data.end();
+  }
 
-  public:
-    array2D(size_t x, size_t y) : data(x*y), x_size(x), y_size(y) {}
-    array2D(size_t x, size_t y, T val) : data(x*y, val), x_size(x), y_size(y) {}
-    
-    T& get(size_t x, size_t y) {
-        return data[x*y_size+x]; 
-    }
-    
-    const T& get(size_t x, size_t y) const{
-        return data[x*y_size+x];
-    }
-    
-    iterator begin(){
-        return data.begin();
-    }
-    
-    iterator end(){
-        return data.end();
-    }
-    
-    const_iterator begin() const{
-        return data.begin();
-    }
-    
-    const_iterator end() const{
-        return data.end();
-    }
-}
+  size_t getSizeX() const {
+    return x_size;
+  }
 
+  size_t getSizeY() const {
+    return y_size;
+  }
 
+  std::tuple<size_t, size_t> getSizes() const {
+    return {x_size, y_size};
+  }
 
-template <class T, size_t DIM>
-class arrayND{
-    std::array<size_t, DIM> sizes;
-    std::array<size_t, DIM-1> access_multiplier;
-    vector<T> data;
-    
-    //constructor
-
-    //get function
-
-  public:
-    array2D(size_t x, size_t y) : data(x*y), x_size(x), y_size(y) {}
-    array2D(size_t x, size_t y, T val) : data(x*y, val), x_size(x), y_size(y) {}
-    
-    template <typename... Args>
-    T& get(Args&&... args)
-    T& get(size_t x, size_t y) {
-        return data[x*y_size+x]; 
-    }
-}
+  size_t size() const {
+    return x_size*y_size;
+  }
+};
